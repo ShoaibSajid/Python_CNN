a8b96aeb00816722a9c275d75619f8d2bf3d21fe
Use_git_config_user.name_without_--global_flag <Use_git_config_user.name_without_--global_flag>
2023-12-04 11:57:51 +0900

    Add yolov2_tiny file

--- yolov2_tiny.py
+++ yolov2_tiny.py
@@ -9,7 +9,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
 
-from loss import build_target, yolo_loss
+# from loss import build_target, yolo_loss
 
 
 class Yolov2(nn.Module):
